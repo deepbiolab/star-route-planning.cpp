e71b9f49a6d32f88986f94f530bb54ce81ad727d
deepbiolab <lin_xiangtian@outlook.com>
2024-12-01 18:42:04 +0800

    Update Search function

--- dev/02-a-star-from-scratch/01-starting-astar-search/main.cpp
+++ dev/02-a-star-from-scratch/01-starting-astar-search/main.cpp
@@ -9,3 +9,93 @@
 Author: Tim Lin
 Date: December 1, 2024
 */
+
+
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+
+using std::vector;
+using std::string;
+using std::cout;
+using std::ifstream;
+using std::istringstream;
+using std::abs;
+
+
+enum class State {kEmpty, kObstacle};
+
+
+// Write the Search function stub here.
+vector<vector<State>> Search(const vector<vector<State>>& grid, const int init[2], const int goal[2]) {
+	cout << "No path found!" << "\n";
+	return vector<vector<State>> {};
+}
+
+
+string CellString(const State& cell) {
+
+	switch (cell) {
+		case State::kObstacle: return "⛰️   ";
+		default: return "0   ";
+	}
+}
+
+
+vector<State> ParseLine(const string& line) {
+	vector<State> row;
+	int n;
+	char c;
+
+	istringstream sline(line);
+	while (sline >> n >> c && c == ',') {
+		if (n == 0) {
+			row.push_back(State::kEmpty);
+		}
+		else {
+			row.push_back(State::kObstacle);
+		}
+		
+	}
+	return row;
+}
+
+
+vector<vector<State>> ReadBoardFile(const string& path) {
+	vector<vector<State>> board;
+	
+	ifstream my_file(path);
+	string line;
+	if (my_file) {
+		while (getline(my_file, line)) {
+			board.push_back(ParseLine(line));
+		}
+	}
+	return board;
+}
+
+
+void PrintBoard(const vector<vector<State>>& board) {
+	for (const auto& row : board) {
+		for (const auto& col : row) {
+			cout << CellString(col);
+		}
+		cout << "\n";
+	}
+}
+
+
+int main() {
+  // Declare "init" and "goal" arrays with values {0, 0} and {4, 5} respectively.
+  int init[]{0, 0};
+  int goal[]{4, 5};
+  auto board = ReadBoardFile("1.board");
+  
+  // Call Search with "board", "init", and "goal". Store the results in the variable "solution".
+  auto solution = Search(board, init, goal);
+
+  PrintBoard(solution);
+}
